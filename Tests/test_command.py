3d49e22410efdc165d238d934c6c65aecef98cb0
otonvm <otonvm@gmail.com>
2016-06-05 11:36:20 +0200

    started Command class

---
+++ Tests/test_command.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from parallel.main import Command, ExecutableNotFoundError
+
+def test_init():
+    Command()
+
+def test_init_with_something():
+    with pytest.raises(ExecutableNotFoundError):
+        Command("sometext")
+
+def test_init_with_valid():
+    Command("/bin/rm")
+
+def test_path_property_valid():
+    Command().path = "/bin/rm"
+
+def test_path_property_invalid():
+    with pytest.raises(ExecutableNotFoundError):
+        Command().path = "sometext"
